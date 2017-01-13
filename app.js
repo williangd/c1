932c5f6354c2ab70d5f515cb282ffaae2244b93a
Willian Nunes de Oliveira <williangd@yahoo.com.br>
2017-01-12 23:17:00 -0200

    add github service

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 var app = angular.module('myApp', []);
 
-var MainController = function($scope,$http) {
-    var onComplete = function(response) {
-        $scope.user = response.data;
-        $http.get($scope.user.repos_url)
-            .then(onRepos, onError);
+var MainController = function($scope,github,$interval,$log,$anchorScroll,$location) {
+    var onComplete = function(data) {
+        $scope.user = data;
+        github.getRepos($scope.user).then(onRepos, onError);
         $scope.error = "";
     };
 
-    var onRepos = function(response) {
-        $scope.repos = response.data;
-    }
+    var onRepos = function(data) {
+        $scope.repos = data;
+        $location.hash("userDetails");
+        $anchorScroll();
+    };
 
     var onError = function(reason) {
         $scope.error = "Deu ruim!"
-    }
+    };
     
-    $scope.search = function(username) {
-        $http.get("https://api.github.com/users/" + username)
-        .then(onComplete, onError);
+    var decrementCountdown = function(){
+        $scope.countdown -= 1;
+        if($scope.countdown < 1) {
+            $scope.search($scope.username);
+        }
+    };
+    var countdownInterval =null;
+    var startCountdown = function() {
+        countdownInterval =  $interval(decrementCountdown, 1000, $scope.countdown);
     }
+
+    $scope.search = function(username) {
+        $log.info("Searching for " + username);
+        github.getUser(username).then(onComplete, onError);
+        if(countdownInterval) {
+            $interval.cancel(countdownInterval);
+            $scope.countdown = "";
+        }
+    };
     
     $scope.username = "angular";
     $scope.message = "GitHub Viewer";
     $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
 
 };
 
-app.controller('MainController', ['$scope','$http', MainController]);
+app.controller('MainController', MainController);
